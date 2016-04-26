2f26386205971bc208a83a2b464ed60cfab9fc73
Megan Ford <meg.c.ford@gmail.com>
2016-04-26 15:37:04 -0500

    the main things in the javascript are done.

--- translator.js
+++ translator.js
@@ -1,31 +1,46 @@
-var translateButton = document.getElementById("button");
+
+var beginningText = document.getElementById("beginningText")
+var translateButton = document.getElementById("translateButton");
 var translatedTextBox = document.getElementById("translatedText");
 
-var translate = (function(){
-
-  return {
-
-    print: function(event){
+var print = function(event){
+       //put this variable inside the function so it doesn't get assigned until the button is clicked!
       var langOptions = document.getElementById("langOptions").value;
 
+      //statement to determine which language option has been chosen
       if (langOptions === "Spanish") {
-          translatedTextBox.innerHTML = "Spanish";
+          translate.setSpanish(beginningText);
+          translatedTextBox.innerHTML = translate.getSpanish();
           console.log("Spanish!");
       } else if (langOptions === "Dutch") {
-          translatedTextBox.innerHTML = "Dutch";        
+          translatedTextBox.innerHTML = "Dutch placeholder";        
           console.log("Dutch!"); 
       } else if (langOptions ==="Japanese") { 
-          translatedTextBox.innerHTML = "Japanese";
+          translatedTextBox.innerHTML = "Japanese placeholder";
           console.log("Japanese!");
       } else if (langOptions === "Pig-Latin") {
-          translatedTextBox.innerHTML = "Pig-Latin";
+          translatedTextBox.innerHTML = "Pig-Latin placeholder";
           console.log("Pig-Latin!");
       } else {
           alert("HEY, dumbass, pick a language");
       }
-    }
+    };
+
+
+var translate = (function(){
+  var translatedText = ""
+
+  return {
+    setSpanish: function () {
+      //get beginning text. probably a var textToTranslate = beginningText.innerHTML. 
+      //do some stuff like .split to make it an array, .map to translate, and then blah all the stuff. 
+      translatedText = beginningText.innerHTML;//this is just to demonstrate, the translated text will be the result of all the stuff. 
+    },
+    getSpanish: function() {
+      return translatedText;
+    } 
   }
 
 }());
 
-translateButton.addEventListener("click", translate.print);
+translateButton.addEventListener("click", print);
