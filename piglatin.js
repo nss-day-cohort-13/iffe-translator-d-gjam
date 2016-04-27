e5a690d017dbcac86218529482e5daa98bd7345f
Glenn Spinner <glenngineer1@gmail.com>
2016-04-27 13:04:47 -0500

    changes to js file, not linked to main js file yet

--- piglatin.js
+++ piglatin.js
@@ -1,21 +1,31 @@
 var translate = (function(newTranslate) {
+  var translatedPigLatin = "";
 
   var pigLatin = {
-    merry: "errymay",
-    christmas: "istmaschray",
+    Merry: "Errymay",
+    Christmas: "Istmaschray",
     and: "andyay",
-    happy: "appyhay",
-    new: "ewnay",
-    year: "earyay"
+    Happy: "Appyhay",
+    New: "Ewnay",
+    Year: "Earyay"
   };
 
+  return {
 
+    setPigLatin: function(beginningText) {
 
+      var englishPhrase = beginningText.split(" ");
+      console.log("User Inputs", englishPhrase.value);
 
+      englishPhrase.forEach (function(individualWord) {
+        translatedPigLatin += `${pigLatin[individualWord]}`;
+        console.log("pig latin variable", translatedPigLatin);
+      })
+    },
 
+    getPigLatin: function() {
+      return translatedPigLatin;
+    }
+  }
+}(translate));
 
-
-
-
-
-})(translate);
